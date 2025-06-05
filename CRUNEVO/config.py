68b97e663286c581234bf50f21c4ac699ddf1b74
RoggerMS <raongta@gmail.com>
2025-06-05 13:26:09 -0500

    Update config.py

--- CRUNEVO/config.py
+++ CRUNEVO/config.py
@@ -1,7 +1,10 @@
-
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    SECRET_KEY = os.getenv("SECRET_KEY", "clave_segura_por_defecto")
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI", "sqlite:///instance/crunevo.sqlite3")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    DEBUG = os.getenv("DEBUG", "0") == "1"
