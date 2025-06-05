a946aaadde110ad68eddea8d478fe47b1faddadd
RoggerMS <raongta@gmail.com>
2025-06-05 14:07:32 -0500

    Update run.py

--- CRUNEVO/run.py
+++ CRUNEVO/run.py
@@ -1,6 +1,8 @@
+import os
 from crunevo import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)  # Render escaneará este puerto por defecto
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
