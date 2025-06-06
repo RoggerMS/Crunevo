0b4d503d13779c79e25589102c260fc65949dfd0
RoggerMS <raongta@gmail.com>
2025-06-05 20:41:36 -0500

    Remove nonexistent field filter from market route

--- CRUNEVO/crunevo/routes/market_routes.py
+++ CRUNEVO/crunevo/routes/market_routes.py
@@ -5,7 +5,9 @@ market_bp = Blueprint('market', __name__, url_prefix='/marketplace')
 
 @market_bp.route('/')
 def market_home():
-    productos = Product.query.filter_by(aprobado=True).all()
+    # Retrieve all products without filtering by an "aprobado" field, since
+    # that attribute does not exist in the Product model.
+    productos = Product.query.all()
     return render_template('marketplace/market.html', productos=productos)
 
 @market_bp.route('/producto/<int:id>')
