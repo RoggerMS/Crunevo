e9092e03c88ceb9778d74fd246ef2490313381d1
RoggerMS <raongta@gmail.com>
2025-06-05 20:48:52 -0500

    Add basic navigation and ranking route

--- CRUNEVO/crunevo/app.py
+++ CRUNEVO/crunevo/app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from crunevo.routes.main_routes import main_bp
+from crunevo.routes.auth_routes import auth_bp
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object('crunevo.config.Config')
     app.register_blueprint(main_bp)
+    app.register_blueprint(auth_bp)
     return app
--- CRUNEVO/crunevo/routes/main_routes.py
+++ CRUNEVO/crunevo/routes/main_routes.py
@@ -6,3 +6,8 @@ main_bp = Blueprint('main', __name__)
 @main_bp.route('/')
 def index():
     return render_template('index.html')
+
+
+@main_bp.route('/ranking')
+def ranking():
+    return render_template('ranking.html')
