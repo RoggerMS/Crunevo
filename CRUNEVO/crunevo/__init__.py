9168ef0d28898906c88479d2e9ed1d39ed837139
RoggerMS <raongta@gmail.com>
2025-06-05 20:56:20 -0500

    Register auth blueprint

--- CRUNEVO/crunevo/__init__.py
+++ CRUNEVO/crunevo/__init__.py
@@ -2,6 +2,7 @@
 from flask import Flask
 from .models import db
 from .routes.main_routes import main_bp
+from .routes.auth_routes import auth_bp
 
 def create_app():
     app = Flask(__name__)
@@ -9,5 +10,6 @@ def create_app():
 
     db.init_app(app)
     app.register_blueprint(main_bp)
+    app.register_blueprint(auth_bp)
 
     return app
