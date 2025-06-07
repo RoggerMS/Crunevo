d04ce6a9c5462165fc5f162d8768ad6c4036b0bf
RoggerMS <raongta@gmail.com>
2025-06-07 00:01:19 -0500

    Configure port selection for Railway

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
-from crunevo.app import create_app
+import os
+from crunevo import create_app
 
 app = create_app()
 
-if __name__ == '__main__':
-    import os
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
