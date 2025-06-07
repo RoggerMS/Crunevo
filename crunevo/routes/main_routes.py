343b298cfaed3634e76515c6b6e74629c16b35bc
RoggerMS <raongta@gmail.com>
2025-06-07 01:25:52 -0500

    Add protected feed page

--- crunevo/routes/main_routes.py
+++ crunevo/routes/main_routes.py
@@ -1,12 +1,14 @@
 
-from flask import Blueprint, render_template
-from flask_login import login_required
+from flask import Blueprint, render_template, redirect, url_for
+from flask_login import login_required, current_user
 from crunevo.models.note import Note
 
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
 def index():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.feed'))
     return render_template('index.html')
 
 
@@ -19,7 +21,7 @@ def ranking():
 @login_required
 def feed():
     notes = Note.query.order_by(Note.upload_date.desc()).limit(20).all()
-    return render_template('feed.html', notes=notes)
+    return render_template('feed.html', notes=notes, user=current_user)
 
 
 @main_bp.route('/about')
