49ff584f14c5b1ad53b1891b04fa5dd46086d7e1
RoggerMS <raongta@gmail.com>
2025-06-09 14:51:08 -0500

    [Feat] unify posts and notes feed

--- crunevo/models/post.py
+++ crunevo/models/post.py
@@ -11,6 +11,11 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
 
+    @property
+    def created_at(self):
+        """Alias to unify feed ordering"""
+        return self.timestamp
+
     uploader = db.relationship("User", backref="posts")
 
     def __repr__(self):
